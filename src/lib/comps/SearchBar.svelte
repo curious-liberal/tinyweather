51d207c89f9476fed056936a97f976366da1aaff
Patrick Gower <github@dynasity.com>
2025-11-20 16:13:38 +0000

    Created basic searchbar component which typewrites placeholders as search suggestions

---
+++ src/lib/comps/SearchBar.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { TypeWriter } from "$lib/libs/typewriter"
+	import { onMount } from "svelte"
+
+    const props = $props<{suggestions: string[]}>()
+    const suggestions = props.suggestions
+
+    let placeholder = $state("")
+
+    const updatePlaceholder = (text: string): void => {
+        placeholder = text
+    }
+
+    onMount( () => {
+        const tw = new TypeWriter({
+            content: suggestions,
+            target: updatePlaceholder,
+        })
+
+        tw.write()
+    })
+</script>
+
+<input class="searchbar" type="text" {placeholder}>
