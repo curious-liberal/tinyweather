13acd08e86354c1a27ac29ebcf0091238812f7b5
Patrick Gower <github@dynasity.com>
2025-11-25 22:57:27 +0000

    Added grab weather function

--- src/lib/comps/SearchBar.svelte
+++ src/lib/comps/SearchBar.svelte
@@ -20,6 +20,48 @@
 		boundingbox: [string, string, string, string];
 	}
 
+	export interface WeatherData {
+		hourly: {
+			time: Date[];
+
+			temperature_2m: Float32Array | null;
+			precipitation_probability: Float32Array | null;
+			precipitation: Float32Array | null;
+			relative_humidity_2m: Float32Array | null;
+			dew_point_2m: Float32Array | null;
+			apparent_temperature: Float32Array | null;
+			rain: Float32Array | null;
+			showers: Float32Array | null;
+			snowfall: Float32Array | null;
+			snow_depth: Float32Array | null;
+
+			cloud_cover: Float32Array | null;
+			cloud_cover_low: Float32Array | null;
+			cloud_cover_mid: Float32Array | null;
+			cloud_cover_high: Float32Array | null;
+
+			visibility: Float32Array | null;
+			evapotranspiration: Float32Array | null;
+
+			temperature_180m: Float32Array | null;
+			temperature_120m: Float32Array | null;
+			temperature_80m: Float32Array | null;
+
+			wind_gusts_10m: Float32Array | null;
+
+			wind_direction_180m: Float32Array | null;
+			wind_direction_120m: Float32Array | null;
+			wind_direction_80m: Float32Array | null;
+			wind_direction_10m: Float32Array | null;
+
+			wind_speed_10m: Float32Array | null;
+			wind_speed_80m: Float32Array | null;
+			wind_speed_180m: Float32Array | null;
+			wind_speed_120m: Float32Array | null;
+		};
+	}
+
+
 	// Search query
 	let query = $state("")
 
@@ -113,14 +155,14 @@
 				wind_direction_10m: hourly.variables(26)!.valuesArray(),
 				wind_direction_80m: hourly.variables(27)!.valuesArray(),
 			},
-		};
+		}
 
 		// Pass off to LLM
-
+		interpretWeather(weatherData)
 	}
 
-	const interpretWeather = () => {
-
+	const interpretWeather = (weather: WeatherData) => {
+		console.log("Do something with this weather", weather)
 	}
 
 	// Typewriter placeholder
